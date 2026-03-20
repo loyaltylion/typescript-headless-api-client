10a331559fac599d7817b58bf0887507e52100b8
LoyaltyLion CI <ci@loyaltylion.com>
2026-03-20 10:11:14 +0000

    chore: update generated files from PR #10688

--- src/types/api.ts
+++ src/types/api.ts
@@ -348,7 +348,7 @@ export interface components {
              */
             currency: string;
             /** @description `ISO 639-1` code indicating the language used for any text in this response */
-            language: string;
+            language: unknown;
             program: {
                 /**
                  * @description The name of the program
@@ -2524,6 +2524,8 @@ export interface components {
                 /** @description The predicted number of points contributing to tier progress as of the current tier's expiration date. This is similar to the `now` value, but instead of the window ending at `now`, it ends at the current tier expiration date. This can be used to determine if a customer would be able to renew their current tier when it expires This will be `null` if the membership never expires (e.g. lifetime tiers) */
                 at_expiration: number | null;
             };
+            /** @description The ID of the tier the customer is forecasted to be in at their membership expiration, based on their predicted progress at that time. This can be used to warn customers if they are at risk of being downgraded. Will be `null` if the membership never expires (e.g. lifetime tiers, default tier memberships, or conditional tier memberships) */
+            forecasted_tier_id: number | null;
         };
         /** Spend */
         TierProgressSpend: {
@@ -2549,6 +2551,8 @@ export interface components {
                 /** @description The predicted amount of spend contributing to tier progress as of the current tier's expiration date. This is similar to the `now` value, but instead of the window ending at `now`, it ends at the current tier expiration date. This can be used to determine if a customer would be able to renew their current tier when it expires This will be `null` if the membership never expires (e.g. lifetime tiers) */
                 at_expiration: components["schemas"]["CurrencyAmount"] | null;
             };
+            /** @description The ID of the tier the customer is forecasted to be in at their membership expiration, based on their predicted progress at that time. This can be used to warn customers if they are at risk of being downgraded. Will be `null` if the membership never expires (e.g. lifetime tiers, default tier memberships, or conditional tier memberships) */
+            forecasted_tier_id: number | null;
         };
         /**
          * Points redemption
@@ -4775,7 +4779,8 @@ export interface components {
          *             "spend_needed_for_upgrade": {
          *               "amount": 800,
          *               "formatted": "$800"
-         *             }
+         *             },
+         *             "forecasted_tier_id": 1
          *           }
          *         },
          *         "birthday": {
@@ -5271,7 +5276,7 @@ export interface components {
              */
             currency: string;
             /** @description `ISO 639-1` code indicating the language used for any text in this response */
-            language: string;
+            language: unknown;
             /** @description The customer matched by `merchant_id`. The customer's `state` property indicates if the customer is a member of the program (`enrolled`), not a member (`guest`), or has been blocked from the program (`blocked`) */
             customer: components["schemas"]["CustomerEnrolled"] | components["schemas"]["CustomerGuest"] | components["schemas"]["CustomerBlocked"];
             /** @description The complete program configuration. This is the same configuration that is returned by the [Get Configuration](/headless-api/2025-06/configuration/get-configuration) endpoint */
@@ -5466,7 +5471,7 @@ export interface components {
              */
             currency: string;
             /** @description `ISO 639-1` code indicating the language used for any text in this response */
-            language: string;
+            language: unknown;
             /** @description The customer matched by `merchant_id`. The customer's `state` property indicates if the customer is a member of the program (`enrolled`), not a member (`guest`), or has been blocked from the program (`blocked`) */
             customer: components["schemas"]["CustomerEnrolled"] | components["schemas"]["CustomerGuest"] | components["schemas"]["CustomerBlocked"];
             /** @description The complete program configuration. This is the same configuration that is returned by the [Get Configuration](/headless-api/2025-06/configuration/get-configuration) endpoint */
@@ -6597,7 +6602,8 @@ export interface components {
          *             "spend_needed_for_upgrade": {
          *               "amount": 800,
          *               "formatted": "$800"
-         *             }
+         *             },
+         *             "forecasted_tier_id": 1
          *           }
          *         },
          *         "birthday": {
@@ -6964,7 +6970,8 @@ export interface components {
          *             "spend_needed_for_upgrade": {
          *               "amount": 800,
          *               "formatted": "$800"
-         *             }
+         *             },
+         *             "forecasted_tier_id": 1
          *           }
          *         },
          *         "birthday": {
@@ -7286,7 +7293,8 @@ export interface components {
          *             "spend_needed_for_upgrade": {
          *               "amount": 800,
          *               "formatted": "$800"
-         *             }
+         *             },
+         *             "forecasted_tier_id": 1
          *           }
          *         },
          *         "birthday": {
@@ -7608,7 +7616,8 @@ export interface components {
          *             "spend_needed_for_upgrade": {
          *               "amount": 800,
          *               "formatted": "$800"
-         *             }
+         *             },
+         *             "forecasted_tier_id": 1
          *           }
          *         },
          *         "birthday": {
@@ -7930,7 +7939,8 @@ export interface components {
          *             "spend_needed_for_upgrade": {
          *               "amount": 800,
          *               "formatted": "$800"
-         *             }
+         *             },
+         *             "forecasted_tier_id": 1
          *           }
          *         },
          *         "birthday": {
@@ -8252,7 +8262,8 @@ export interface components {
          *             "spend_needed_for_upgrade": {
          *               "amount": 800,
          *               "formatted": "$800"
-         *             }
+         *             },
+         *             "forecasted_tier_id": 1
          *           }
          *         },
          *         "birthday": {
@@ -8556,7 +8567,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -8656,7 +8667,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -8750,7 +8761,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -8845,7 +8856,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -8957,7 +8968,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -9064,7 +9075,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -9170,7 +9181,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -9276,7 +9287,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -9382,7 +9393,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -9488,7 +9499,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -9594,7 +9605,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -9700,7 +9711,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -9806,7 +9817,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -9912,7 +9923,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -10018,7 +10029,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -10124,7 +10135,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -10230,7 +10241,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -10336,7 +10347,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -10442,7 +10453,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -10548,7 +10559,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
@@ -10654,7 +10665,7 @@ export interface operations {
                 /** @description The sales channel from which this request is made. Must be provided as this query parameter, or the `X-LoyaltyLion-Channel` header */
                 channel?: components["schemas"]["SupportedChannel"];
                 /** @description The language to use for the request. If not provided, the site's default language will be used */
-                language?: string;
+                language?: unknown;
             };
             header?: never;
             path: {
